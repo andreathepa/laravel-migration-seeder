d7f856c102848892c2621e61f06ca475ff62d5a3
andreathepa <andreadepa32@gmail.com>
2023-07-28 09:38:54 +0200

    controller + model done

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $train = train::all();
+        return view('home', compact('train'));
+    }
+
+}
+
